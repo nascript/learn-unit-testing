f7d4383a015fd7f19746af13d5aa36aee8891b89
nascript <nascodefy@gmail.com>
2022-07-10 12:28:15 +0700

    jest with verbose

---
+++ README.md
@@ -0,0 +1,7 @@
+## LEARN UNIT TESTING WITH JEST
+
+### Jest tidak support javascript module
+
+<p>oleh karena itu diperlukan babel js untuk membantu jest mengkonversi javascript module Es6 ke javascript versi yang lama yaitu common js </p>
+
+<a>[LINK babel setup](https://babeljs.io/setup#installation)</a>
---
+++ test/sum.test.js
@@ -0,0 +1,14 @@
+import { sum } from '../src/sum'
+
+test("sum(1, 2) must be 3 one", () => {
+  const result = sum(1, 2)
+  expect(result).toBe(3)
+})
+test("sum(1, 2) must be 3 two", () => {
+  const result = sum(1, 2)
+  expect(result).toBe(3)
+})
+test("sum(1, 2) must be 3 three", () => {
+  const result = sum(1, 2)
+  expect(result).toBe(3)
+})
