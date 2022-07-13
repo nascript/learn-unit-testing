cf47af79d002dc7e61f175c7fc2acfc5ff3e5bae
nascript <nascodefy@gmail.com>
2022-07-13 22:20:32 +0700

    async testing day 2

--- test/asyncCode.test.js
+++ test/asyncCode.test.js
@@ -4,3 +4,15 @@ test('test async code', async () => {
   const result = await sayHelloAsync('NAS')
   expect(result).toBe('Hello NAS')
 })
+test('test async code 1', async () => {
+  const result = await sayHelloAsync('NAS')
+  expect(result).toBe('Hello NAS')
+})
+test('test async code 2', async () => {
+  const result = await sayHelloAsync('NAS')
+  expect(result).toBe('Hello NAS')
+})
+test('test async code 3', async () => {
+  const result = await sayHelloAsync('NAS')
+  expect(result).toBe('Hello NAS')
+})
